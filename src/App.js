6a476232805e78a000fc826f9173bf85d0c24475
Jacky Madenfrost <jacky.madenfrost@gmail.com>
2021-10-06 17:42:34 +1000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://jackymadenfrost.com/" target="_blank">
+          Jacky Madenfrost
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/jackymadenfrost/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
