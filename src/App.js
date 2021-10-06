93c108a75b61b6cebc2faf7986413f5a10e1bccb
Jacky Madenfrost <jacky.madenfrost@gmail.com>
2021-10-06 18:02:34 +1000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://jackymadenfrost.com/">Jacky Madenfrost</a> and is{" "}
-        <a href="https://github.com/jackymadenfrost/react-weather-app">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://jackymadenfrost.com/">Jacky Madenfrost</a> and is{" "}
+          <a href="https://github.com/jackymadenfrost/react-weather-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
